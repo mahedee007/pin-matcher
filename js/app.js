0920d0cfadc1485fd185466b3977d88e7ee20033
Mahedee Hasan <mahedee007@gmail.com>
2023-08-06 00:46:19 +0600

    function added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
+function getPin(){
+    const random = generatePin();
+    const randomString = random + '';
+    if (randomString.length === 4){
+        return random;
+    }else{
+        return getPin();
+    }
+
+}
+
+
 function generatePin(){
     const pin = Math.round(Math.random()* 10000);
     return pin;
 }
 document.getElementById('genarate-pin').addEventListener('click', function(){
    const displayPin = document.getElementById('show-pin');
-   displayPin.value = generatePin();
+   displayPin.value = getPin();
+})
+
+document.getElementById('calc-btn').addEventListener('click', function(){
+    const displayNumber = document.getElementById('show-number');
+
 })
